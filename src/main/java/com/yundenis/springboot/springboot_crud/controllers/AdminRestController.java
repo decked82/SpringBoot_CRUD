5049dba5c6257daef400ac488597eb326b03f1d1
Yun Denis <yundenis1982@gmail.com>
2022-02-19 17:40:55 +0900

    создан рестконтроллер

---
+++ src/main/java/com/yundenis/springboot/springboot_crud/controllers/AdminRestController.java
@@ -0,0 +1,29 @@
+package com.yundenis.springboot.springboot_crud.controllers;
+
+import com.yundenis.springboot.springboot_crud.models.User;
+import com.yundenis.springboot.springboot_crud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class AdminRestController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AdminRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/admin")
+    public List<User> getAllUsers() {
+        List<User> allUsers = userService.showAllUsers();
+        return allUsers;
+    }
+
+}
